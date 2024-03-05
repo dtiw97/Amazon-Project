f3b429ff1c65cb14116fedf87721caaba8527d92
tiw <dtiw.dummy@gmail.com>
2024-03-05 16:50:03 +0800

    Jasmine Test on Conversion.

---
+++ test-jasmine/conversion.js
@@ -0,0 +1,17 @@
+import {toDollar} from "../script/utils/conversions.js"
+
+describe('test suite: toDollar', ()=> {
+  it('convert cents into dollar', () => {
+    expect(toDollar(2095)).toEqual('20.95');
+  })
+
+  it('works with 0', () => { 
+    expect(toDollar(0)).toEqual('0.00');
+  })
+
+  it('rounds up to nearest cent', () => {
+    expect(toDollar(2000.5)).toEqual('20.01');
+  })
+
+  
+})
